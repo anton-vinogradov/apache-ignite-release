8e78a77f7ac81017b898a22ef3bc1fc9e26a28ae
Ivanov Petr <pivanov@gridgain.com>
2018-01-23 12:18:21 +0300

    Added RPM package build

--- scripts/vote_3_step_1[rpm]create_rpm_repository.sh
+++ scripts/vote_3_step_1[rpm]create_rpm_repository.sh
@@ -17,19 +17,34 @@ then
     sudo apt-get update
     sudo apt-get install rpm createrepo gnupg-agent --no-install-recommends -y
 fi
+echo
+
+#
+# Build package
+#
+echo "# Building RPM package #"
+if [ ! -f rpmbuild ]; then rm -rf rpmbuild; fi
+mkdir -pv rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+cp -rfv git/packaging/rpm/* rpmbuild
+cp -rfv svn/vote/apache-ignite-fabric-${ignite_version}-bin.zip rpmbuild/SOURCES/apache-ignite.zip
+rpmbuild -bb --define "_topdir $(pwd)/rpmbuild" rpmbuild/SPECS/apache-ignite.spec
+echo
 
 #
 # Prepare repository root
 #
 echo "# Preparing repository root #"
+if [ ! -f rpm ]; then rm -rf rpm; fi
 mkdir -pv ${REPO_ROOT}
-mv -v *.rpm ${REPO_ROOT}
+mv -v rpmbuild/RPMS/noarch/*.rpm ${REPO_ROOT}
+echo
 
 #
 # Sign RPM
 #
 echo "# Signing RPM #"
 rpm --define "_gpg_name $(gpg --list-keys | grep uid | sed -r 's|uid\ +(.*)|\1|')" --addsign ${REPO_ROOT}/*.rpm
+echo
 
 #
 # Create repository layout
